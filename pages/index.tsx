b6e713c697b05079d0e1f58ac848f46456487348
Ilnur <ilnur.bkhtrv@gmail.com>
2021-06-10 22:28:17 +0300

    функция получения staticPops с интерфейсом получаемых данных

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { Htag, Tag, Button, P, Rating } from '../components';
 import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { GetStaticProps } from 'next';
+import { MenuItem } from '../interfaces/menu.interface';
 
 
-function Home(): JSX.Element {
+
+function Home({ menu }: HomeProps): JSX.Element {
 	const [counter, setCounter] = useState<number>(0);
 
 	const [rating, setRating] = useState<number>(3);
@@ -22,9 +26,32 @@ function Home(): JSX.Element {
 			<Tag size='small' color='red'>large</Tag>
 			<Rating rating={rating} isEditable setRating={setRating} />
 
+			<ul>
+				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+			</ul>
+
 		</>
 	);
 }
 
-export default withLayout(Home);				/*  HOCconponent оборачивает содержимое компонента 
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {				/* функция получения данных с сервера */
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+
+}
+export default withLayout(Home);				/*  HOCconponent оборачивает содержимое компонента,
 													export default начало рендера страницы */
